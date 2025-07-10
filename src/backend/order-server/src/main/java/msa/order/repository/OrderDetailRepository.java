fe34b1ebb1e1047376ef74235a04daee3cec1d0e
dhwoo <wooda01@naver.com>
2025-07-10 18:28:54 +0900

    [BE] remove: OrderDetailRepository 삭제 #7

--- src/backend/order-server/src/main/java/msa/order/repository/OrderDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package msa.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import msa.order.entity.OrderDetail;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
